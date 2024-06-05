87a6e546d2e3a6e429350f342abbb86782f621fb
ORADEV <omotosoabiola9525@gmail.com>
2024-06-05 23:32:56 +0100

    Lesson 2

---
+++ 2.Reading_from_SOL_BC/check_balance.ts
@@ -0,0 +1,13 @@
+import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+
+const publicKey = new PublicKey("2kXkxr1tc1sYCfULwfNKNrYsNP2XqvqYdBqZE9UGZ4hC")
+
+const connection = new Connection("https://api.devnet.solana.com", "confirmed")
+
+const balanceInLamports = await connection.getBalance(publicKey);
+
+const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+console.log(
+    `💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL} $SOL!`
+  );
