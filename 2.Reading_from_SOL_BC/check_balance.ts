24ca5d8f47318199ec41da8ec9242f8b839b9e06
ORADEV <omotosoabiola9525@gmail.com>
2024-06-05 23:36:54 +0100

    Lesson 2

--- 2.Reading_from_SOL_BC/check_balance.ts
+++ 2.Reading_from_SOL_BC/check_balance.ts
@@ -8,6 +8,7 @@ const balanceInLamports = await connection.getBalance(publicKey);
 
 const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
 
+console.log(`Balance: ${balanceInSOL} SOL`);
 console.log(
     `💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL} $SOL!`
   );
